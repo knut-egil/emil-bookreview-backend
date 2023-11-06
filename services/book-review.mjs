bea3cd68006f6d7f63244767dab5bd3933c78ac8
Knut Egil V. N. <86576502+knut-egil@users.noreply.github.com>
2023-11-06 17:26:39 +0000

    Add error handling

--- services/book-review.mjs
+++ services/book-review.mjs
@@ -24,43 +24,49 @@ function isCacheStale(key) {
  * @returns 
  */
 async function getBookReviews(useCache = true) {
-  const CACHE_KEY = "bookreviews";
+  try {
+    const CACHE_KEY = "bookreviews";
 
-  // Get cached data
-  let bookReviews = cache[CACHE_KEY].data;
+    // Get cached data
+    let bookReviews = cache[CACHE_KEY].data;
 
-  // Check if data is stale
-  if (isCacheStale(CACHE_KEY) || !useCache) {
-    // Send HTTP request for å hente data
-    const res = await fetch("https://script.google.com/macros/s/AKfycby7YloHJdUYF1y1IMzaDbTd8p13qiHEvTr9OctGlCZ8SIRAVhIpeI3GgqvgD0U1cJDO/callback", {
-        "headers": {
-        "accept": "*/*",
-        "accept-language": "en-US,en;q=0.5",
-        "cache-control": "no-cache",
-        "content-type": "application/x-www-form-urlencoded;charset=UTF-8",
-        "pragma": "no-cache",
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "same-origin",
-        "x-same-domain": "1"
-      },
-      "body": "request=%5B%22getData%22%2C%22%5B%5D%22%2Cnull%2C%5B0%5D%2Cnull%2Cnull%2C1%2C0%5D",
-      "method": "POST",
-      "credentials": "omit"
-    });
+    // Check if data is stale
+    if (isCacheStale(CACHE_KEY) || !useCache) {
+      // Send HTTP request for å hente data
+      const res = await fetch("https://script.google.com/macros/s/AKfycby7YloHJdUYF1y1IMzaDbTd8p13qiHEvTr9OctGlCZ8SIRAVhIpeI3GgqvgD0U1cJDO/callback", {
+          "headers": {
+          "accept": "*/*",
+          "accept-language": "en-US,en;q=0.5",
+          "cache-control": "no-cache",
+          "content-type": "application/x-www-form-urlencoded;charset=UTF-8",
+          "pragma": "no-cache",
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-origin",
+          "x-same-domain": "1"
+        },
+        "body": "request=%5B%22getData%22%2C%22%5B%5D%22%2Cnull%2C%5B0%5D%2Cnull%2Cnull%2C1%2C0%5D",
+        "method": "POST",
+        "credentials": "omit"
+      });
 
-    // Les data fra respons, som tekst og fjern noen tegn fra starten
-    const data = JSON.parse((await res.text()).slice(6));
+      // Les data fra respons, som tekst og fjern noen tegn fra starten
+      const data = JSON.parse((await res.text()).slice(6));
 
-    // Hent ut bok review dataen fra respons-data
-    bookReviews = JSON.parse(data[0][1][1]);
+      // Hent ut bok review dataen fra respons-data
+      bookReviews = JSON.parse(data[0][1][1]);
 
-    // Cache the data
-    cacheData(CACHE_KEY, bookReviews);
+      // Cache the data
+      cacheData(CACHE_KEY, bookReviews);
+    }
+
+    // Return 
+    return bookReviews;
   }
-
-  // Return 
-  return bookReviews;
+  catch(err) {
+    console.error(err);
+    return err;
+  }
 }
 
 export default {

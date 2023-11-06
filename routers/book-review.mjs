7a1e7dff3501de48bd88912e3fd5fa149a538254
Knut Egil V. N. <86576502+knut-egil@users.noreply.github.com>
2023-11-06 16:43:06 +0000

    Add refresh task start and status endpoints

--- routers/book-review.mjs
+++ routers/book-review.mjs
@@ -5,7 +5,7 @@ import BookReviewService from "../services/book-review.mjs";
 const router = new Router();
 
 // Set up "/all" route
-router.get("/all", async function (req,res) {
+router.get("/all", async function (req, res) {
     // Parse "useCache" query param
     const useCache = (["true","1"].some(v=>v==req.query?.cache?.toLowerCase())) || true;
 
@@ -16,8 +16,63 @@ router.get("/all", async function (req,res) {
     res.json(bookReviews);
 });
 
-router.use("*", function (req,res){
+// Start bookreviews refresh/fetch task
+const bookReviewsTask = {
+    promise: null,
+    startTimestamp: null,
+    finishTimestamp: null
+};
+function promiseState(p) {
+    if (p == null)
+        return "not started";
+    const t = {};
+    return Promise.race([p, t])
+        .then(v => (v === t)? "pending" : "fulfilled", () => "rejected");
+}
+
+// Get bookReviews refresh task status
+router.get("/refresh", async function (req, res) {
+    // Check current promise state
+    const taskState = promiseState(bookReviewsTask.promise);
+
+    // Return
+    res.json({
+        state: taskState,
+    })
+});
+// Start bookReviews refresh task
+router.put("/refresh", async function (req, res) {
+    const taskState = promiseState(bookReviewsTask.promise);
+    if (taskState == "pending") {
+        return res.status(409)
+        .json({
+            success: false,
+            state: taskState,
+            message: "Existing refresh task is pending."
+        });
+    }
+
+    // Initiate BookReviewService.getBookReviews
+    const getBookReviewsPromise = BookReviewService.getBookReviews(false);;
+    bookReviewsTask.promise = getBookReviewsPromise;
+    bookReviewsTask.startTimestamp = Date.now();
+    bookReviewsTask.finishTimestamp = null;
+
+    // Set finish timestamp on finally
+    getBookReviewsPromise.finally(function() {
+        bookReviewsTask.finishTimestamp = Date.now();
+    });
+
+    // Return status or something
+    res.json({
+        success: true,
+        state: taskState,
+        message: "Refresh task started."
+    });
+});
+
+router.use("*", function (req, res) {
     res.send("Available endpoints:</br>/all");
-})
+});
 
 export default router;
